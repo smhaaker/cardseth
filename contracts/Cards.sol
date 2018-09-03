5d025bc3618fd122bdfff7a6e7e9ef7ef5ed268c
smhaaker <steffen.haaker@gmail.com>
2018-09-02 22:37:03 -0400

    Added and Updated Contracts, Fixed Deployment to Include Cards Contract

---
+++ contracts/Cards.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.15;
+
+contract Cards {
+
+    string public url;
+
+    event urlEvent(string url, address indexed _from);
+    
+    function setUrl(string _newUrl) public {
+        url = _newUrl;
+        emit urlEvent(_newUrl, msg.sender);
+    }
+
+    function getUrl() public constant returns (string)
+    { return url;}
+
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Users = artifacts.require("./Users.sol");
+var Cards = artifacts.require("./Cards.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Users);
+  deployer.deploy(Cards);
 };
